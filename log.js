7a5ffb44f797dabefc7d3f95524c7358b449c705
kripa <kripapokharel7@gmail.com>
2024-05-10 09:54:29 +0545

    first commit

---
+++ log.js
@@ -0,0 +1,11 @@
+const newAccount=()=>{
+    document.getElementById("newAccount").innerHTML="Email or phone number"
+    document.getElementById("signIn").innerHTML="Password"
+
+}
+
+const signIn=()=>{
+    document.getElementById("formCreate").style.display="block"
+    
+
+}
